e2afe2fdba1186fb74d46f3151c4163568b87175
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 15:41:01 +0530

    Initial commit

---
+++ stringarray/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Create an array with six strings.
+            string[] array = new string[6];
+            array[0] = "chittu";
+            array[1] = "chotu";
+            array[2] = "motu";
+            array[3] = "patlu";
+            array[4] = "chintu";
+            array[5] = "bittu";
+
+            // Call the methods.
+            string result1 = ConvertStringArrayToString(array);
+            string result2 = ConvertStringArrayToStringJoin(array);
+
+            // Display the results.
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+        }
+
+        static string ConvertStringArrayToString(string[] array)
+        {
+            // Concatenate all the elements into a StringBuilder.
+            StringBuilder builder = new StringBuilder();
+            foreach (string value in array)
+            {
+                builder.Append(value);
+                builder.Append('.');
+            }
+            return builder.ToString();
+        }
+
+        static string ConvertStringArrayToStringJoin(string[] array)
+        {
+            // Use string Join to concatenate the string elements.
+            string result = string.Join(".", array);
+            return result;
+        }
+    }
+}
+
+
+
+
+
+
